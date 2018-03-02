42ccfc0929b769f27d81519ba0c176de6896b40e
Santiago Sanchez Gomez <chadsfather@gmail.com>
2018-03-02 23:58:21 +0100

    Update firebase-client.js

--- plugins/firebase/firebase-client.js
+++ plugins/firebase/firebase-client.js
@@ -2,7 +2,7 @@ import * as firebasedb from 'firebase'
 
 if (!firebasedb.apps.length) {
     firebasedb.initializeApp({
-		apiKey: 'AIzaSyDs4BHqc_m6enr0tZl7M28x2rWZ_oY7Jg4',
+		apiKey: 'abcdefghijklmnopqrst',
 		databaseURL: 'https://test-3c452.firebaseio.com/',
 	})
 }
